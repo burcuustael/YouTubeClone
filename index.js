896dbcd5a733c3baa62ce072f09c0ae43ae43082
burcuustael <burcustael@gmail.com>
2024-07-26 19:40:05 +0300

    youtube api den veriler çekildi ve tamamlandı.

---
+++ index.js
@@ -0,0 +1,68 @@
+const videoContainer = document.querySelector(".video-container");
+
+let api_key = "AIzaSyBYpq8pukt_6lJ6RBiQUP0EGhEm1ZhqV6Q";
+let video_http = "https://www.googleapis.com/youtube/v3/videos?";
+let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
+
+fetch(
+  video_http +
+    new URLSearchParams({
+      key: api_key,
+      part: "snippet",
+      chart: "mostPopular",
+      maxResults: 50,
+      regionCode: "TR",
+    })
+)
+  .then((res) => res.json())
+  .then((data) => {
+    data.items.forEach((item) => {
+      getChannelIcon(item);
+    });
+  })
+  .catch((err) => console.log(err));
+
+const getChannelIcon = (video_data) => {
+  fetch(
+    channel_http +
+      new URLSearchParams({
+        key: api_key,
+        part: "snippet",
+        id: video_data.snippet.channelId,
+      })
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      video_data.channelThumbnail =
+        data.items[0].snippet.thumbnails.default.url;
+      makeVideoCard(video_data);
+    });
+};
+
+const makeVideoCard = (data) => {
+  videoContainer.innerHTML += `
+    <div class="video" onclick="location.href = 'https://youtube.com/watch?v=${data.id}'">
+        <img src="${data.snippet.thumbnails.high.url}" alt="" class="thumbnail" />
+        <div class="content">
+          <img src="${data.channelThumbnail}" class="channel-icon" alt="" />
+          <div class="info">
+            <h4 class="title">${data.snippet.title}</h4>
+            <p class="channel-name">${data.snippet.channelTitle}</p>
+          </div>
+        </div>
+      </div>
+`;
+};
+
+//search Logic
+
+const searchInut = document.querySelector(".search-bar");
+const searchBtn = document.querySelector(".search-btn");
+
+let searchLink = "https://wwww.youtube.com/results?search_query=";
+
+searchBtn.addEventListener("click", () => {
+  if (searchInut.value.length) {
+    location.href = searchLink + searchInut.value;
+  }
+});
